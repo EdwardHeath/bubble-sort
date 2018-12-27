455622e1edc0c111f6948ecb623953abe1deead8
Edward Heath <e42291+github@gmail.com>
2018-12-26 18:12:36 -0800

    Complete bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,55 @@
-print "Enter array: "
-arr = gets.chomp[1..-2].split(",").map(&:to_i)
+arr1 = [4,3,78,2,0,2]
+arr2 = ["hi","hello","hey"]
 
-len = arr.size
-loop do
-    swapped = false
-    (len-1).times do |i|
-        if(arr[i] > arr[i+1])
-            x = arr[i+1]
-            arr[i+1] = arr[i]
-            arr[i] = x
-            swapped = true
+def bubble_sort arr1
+    arr = arr1
+
+    puts "=== BUBBLE SORT ==="
+    puts "Unsorted Array: #{arr.inspect}"
+
+    len = arr.size
+    loop do
+        swapped = false
+        (len-1).times do |i|
+            if(arr[i] > arr[i+1])
+                x = arr[i+1]
+                arr[i+1] = arr[i]
+                arr[i] = x
+                swapped = true
+            end
         end
-    end
-    break if not swapped
-end 
+        break if not swapped
+    end 
 
-puts arr.inspect
+    puts "Sorted Array: #{arr.inspect}"
+    puts
+end
+
+def bubble_sort_by arr2
+    arr = arr2
+
+    puts "=== BUBBLE SORT BY ==="
+    puts "Unsorted Array: #{arr.inspect}"
+
+    len = arr.size
+    loop do
+        swapped = false
+        (len-1).times do |i|
+            if(yield(arr[i],arr[i+1]) > 0)
+                x = arr[i+1]
+                arr[i+1] = arr[i]
+                arr[i] = x
+                swapped = true
+            end
+        end
+        break if not swapped
+    end 
+
+    puts "Sorted Array by length: #{arr.inspect}"
+    puts
+end
+
+bubble_sort(arr1)
+bubble_sort_by(arr2) do |left, right|
+    left.length - right.length
+end
